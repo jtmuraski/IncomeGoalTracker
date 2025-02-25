99a7232eba9f73c68bca50e161575023109634f9
John Muraski <john.t.muraski@outlook.com>
2025-02-25 16:47:08 -0600

    Updates

---
+++ IncomeGoalTracker.Core/Models/Ceu/ClassCeuUi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncomeGoalTracker.Core.Models.Ceu
+{
+    public class ClassCeuUi
+    {
+        // UI Class for ClassCeu
+        public int Id { get; set; }
+        [DisplayName("Certificate Name")]
+        public int CertificateId { get; set; }
+        [DisplayName("CEU Hours")]
+        [Range(0.1, 100.00, ErrorMessage = "CEU hours must be between 0.1 and 100")]
+        public double CeuHours { get; set; }
+    }
+}
