b48cfe34fdc802307f0bad22c2a6c510866652de
John Muraski <john.t.muraski@outlook.com>
2025-03-13 16:42:59 -0500

    Starting more repos

---
+++ IncomeGoalTracker.Data/Repositories/TrainingClassRepo.cs
@@ -0,0 +1,38 @@
+﻿using IncomeGoalTracker.Core.Interfaces;
+using IncomeGoalTracker.Core.Models.Ceu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncomeGoalTracker.Data.Repositories
+{
+    public class TrainingClassRepo : ITrainingClassRepo
+    {
+        public Task<int> AddTrainingClassAsync(TrainingClass trainingClass)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteTrainingClassAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TrainingClass>> GetAllTrainingClassesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TrainingClass> GetTrainingClassByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateTrainingClassAsync(TrainingClass trainingClass)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
