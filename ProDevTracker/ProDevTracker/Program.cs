7f71056dfd659623f4ef9959f377f65abe781cb0
John Muraski <john.t.muraski@outlook.com>
2025-04-01 22:37:21 -0500

    UI Changes
    Finished switching to Radzen Blazor

--- ProDevTracker/ProDevTracker/Program.cs
+++ ProDevTracker/ProDevTracker/Program.cs
@@ -1,7 +1,14 @@
 using ProDevTracker.Client.Pages;
 using ProDevTracker.Components;
+using Microsoft.Data.SqlClient;
 using Radzen;
 using Radzen.Blazor;
+using IncomeGoalTracker.Data;
+using IncomeGoalTracker.Data.Repositories;
+using IncomeGoalTracker.Core.Interfaces;
+using IncomeGoalTracker.Core.Services.Implementations;
+using IncomeGoalTracker.Core.Services.Interfaces;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +19,28 @@ builder.Services.AddRazorComponents()
 
 // Add 3rd party UI services
 builder.Services.AddRadzenComponents();
+
+// Budiness and database logic services
+builder.Services.AddSingleton<IDbConnectionFactory>(c => new SqlConnectionFactory(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ICertificateRepo, CertificateRepo>();
+
+
+builder.Services.AddScoped<ICertificateService, CertificateService>();
+
+// Add Logging services
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddLogging(logging =>
+    {
+        logging.AddConsole();
+        logging.AddDebug();
+    });
+}
+else
+{
+    builder.Services.AddApplicationInsightsTelemetry();
+}
 
 var app = builder.Build();
 
